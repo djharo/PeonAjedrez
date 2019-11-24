a84ded9ad872af31570a30b2ae4905e5e03e6607
DiegoJ <DiegoJ@DESKTOP-64OLSOS>
2019-11-24 18:35:43 +0100

    Creacion get y set Peon

--- src/main/java/org/iesalandalus/programacion/peonajedrez/Peon.java
+++ src/main/java/org/iesalandalus/programacion/peonajedrez/Peon.java
@@ -1,5 +1,30 @@
 package org.iesalandalus.programacion.peonajedrez;
 
 public class Peon {
+	
+	private Color color;
+	
+	private Posicion posicion;
 
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		
+		if (color == null) {
+			throw new IllegalArgumentException("ERROR: No se puede asignar un color nulo.");
+		}
+		
+		this.color = color;
+	}
+
+	public Posicion getPosicion() {
+		return new Posicion(posicion);
+	}
+
+	public void setPosicion(Posicion posicion) {
+		
+		this.posicion = new Posicion(posicion);
+	}
 }
